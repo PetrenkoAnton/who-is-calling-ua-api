fff5416ba814f29826d8936833fbf1be5f9bb2d4
antonpetrenko <anton.petrenko@business-class.com>
2023-11-24 22:48:40 +0200

    Updated SearchService

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -3,13 +3,28 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use DiDom\Document;
+use DiDom\Element;
+
 class SearchService
 {
     public function search(string $number): array
     {
+        $outputComments = [];
+
+        $url = 'https://www.telefonnyjdovidnyk.com.ua/nomer/' . $number;
+        $document = new Document($url, true);
+
+        $comments = $document->find('.comment-item .comment .comment-text');
+
+        foreach ($comments as $comment) {
+            /** @var $comment Element */
+            $outputComments[] = $comment->text();
+        }
+
         return [
             'phone' => $number,
-            'status' => 123,
+            'comments' => $outputComments,
         ];
     }
 }
