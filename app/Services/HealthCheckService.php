14494aa7760ca4abb7441333e2ccb803d833d842
antonpetrenko <anton.petrenko@business-class.com>
2023-12-12 15:42:04 +0200

    Added redis cache

--- app/Services/HealthCheckService.php
+++ app/Services/HealthCheckService.php
@@ -3,11 +3,22 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
+
 class HealthCheckService
 {
     public function check(): bool
     {
-        // TODO! Add some health check implementation
-        return true;
+        return $this->checkCache();
+    }
+
+    private function checkCache(): bool
+    {
+        $key = 'health-check';
+        $input = time();
+
+        Cache::put($key, $input);
+
+        return $input === Cache::pull($key);
     }
 }
--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -5,26 +5,31 @@ namespace App\Services;
 
 use DiDom\Document;
 use DiDom\Element;
+use DiDom\Exceptions\InvalidSelectorException;
+use JetBrains\PhpStorm\ArrayShape;
 
 class SearchService
 {
+    /**
+     * @throws InvalidSelectorException
+     */
+    #[ArrayShape(['phone' => "string", 'comments' => "array", 'from_cache' => "boolean"])]
     public function search(string $number): array
     {
         $outputComments = [];
-
         $url = 'https://www.telefonnyjdovidnyk.com.ua/nomer/' . $number;
-        $document = new Document($url, true);
 
+        $document = new Document($url, true);
         $comments = $document->find('.comment-item .comment .comment-text');
 
-        foreach ($comments as $comment) {
+        foreach ($comments as $comment)
             /** @var $comment Element */
             $outputComments[] = $comment->text();
-        }
 
         return [
             'phone' => $number,
             'comments' => $outputComments,
+            'from_cache' => false,
         ];
     }
 }
